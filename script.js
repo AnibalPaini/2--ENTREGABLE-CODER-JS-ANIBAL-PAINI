09be569502c7008f882511b07f384e9105556179
Anibal Paini <anibalpaini01@gmail.com>
2024-11-15 00:33:41 -0300

    Guardamos cambios

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //MENU HAMBURGUESA  --HECHO EN EL CURSO DE DESARROLLO WEB
 const hamburguesa= document.querySelector('.header__menu')
 hamburguesa.onclick=function(){
@@ -7,6 +6,7 @@ hamburguesa.onclick=function(){
     hamburguesa.classList.toggle("active");
 }
 
+const cantidadCarrito=document.getElementById("cantidad-carrito");
 const contenedorProducto = document.getElementById('contenedor-productos'); // DIV CONTENEDOR DE LOS PRODUCTOS
 const gridProducto = document.createElement('div');
 gridProducto.classList.add('productos'); // DIV CON GRID 
@@ -22,7 +22,7 @@ function cargarCarritoDesdeLocalStorage() {
     }
 }
 
-fetch("./data.json")
+fetch("./data.json") //Mostramos los productos
 .then(response=> response.json())
 .then(productos => {
     productos.forEach(producto => {
@@ -45,13 +45,24 @@ fetch("./data.json")
 
 function agregarAlCarrito(producto) { // FUNCIONALIDAD AGREGAR PRODUCTO AL CARRITO
     const itemEnCarrito = carrito.find(item => item.nombre === producto.nombre);
-    
     if (itemEnCarrito) {
         itemEnCarrito.cantidad++; // SI YA ESTA AUMENTA LA CANTIDAD
     } else {
         carrito.push({ ...producto, cantidad: 1 }); // SINO AGREGA UN NUEVO PRODUCTO
     }
-
+    Swal.fire({
+        position: "bottom-start",
+        icon: "info",
+        title: "Se agrego al carrito",
+        showConfirmButton: false,
+        timer: 1500,
+        backdrop:false,
+        width:250,
+        customClass: {
+            title: 'sweet-titulo',  
+            icon: 'sweet-icon'  
+        }
+    });
     const carritoContenedor = document.getElementById('carrito-contenedor'); // SE ABRE EL CARRITO CADA VEZ QUE AGREGAMOS UN PRODUCTO
     carritoContenedor.classList.remove('oculto');
 
@@ -60,40 +71,104 @@ function agregarAlCarrito(producto) { // FUNCIONALIDAD AGREGAR PRODUCTO AL CARRI
 
 function actualizarCarrito() { //ACTUALIZAMOS EL CARRITO
     const contenedorCarrito = document.getElementById('carrito');
-    contenedorCarrito.innerHTML = `<h2>Carrito</h2>`; 
+    contenedorCarrito.innerHTML = ``; 
+
+    const divHeaderCarrito=document.createElement(`div`);
+    divHeaderCarrito.classList.add(`headerCarrito`);
+
+    const cerrarCarrito=document.createElement(`p`);
+    cerrarCarrito.textContent=`X`;
+    cerrarCarrito.classList.add(`cerrar-carrito`)
+    divHeaderCarrito.appendChild(cerrarCarrito);
+
+    cerrarCarrito.addEventListener("click",()=>{
+        const carritoContenedor = document.getElementById('carrito-contenedor');
+        carritoContenedor.classList.toggle('oculto'); 
+    });
+
+    const tituloCarrito=document.createElement("h2");
+    tituloCarrito.textContent=`Mi carrito`;
+    tituloCarrito.classList.add(`titulo-carrito`);
+    divHeaderCarrito.appendChild(tituloCarrito);
+    
+
+    contenedorCarrito.appendChild(divHeaderCarrito)
     
     carrito.forEach((producto, index) => {
-        const div = document.createElement('div');
-        div.classList.add('producto-carrito');
+        const divProducto = document.createElement('div');
+        divProducto.classList.add('producto-carrito');
 
         //CREAMOS EL HTML DE LOS PRODUCTOS DENTRO DEL CARRITO
-        div.innerHTML = ` 
-            <img src="${producto.imagen}"> 
-            <p>${producto.nombre} (x${producto.cantidad})</p>
-            <p>$${producto.precio * producto.cantidad}</p> 
-            <button class="eliminar-producto">-</button>
+        divProducto.innerHTML = ` 
+            <div class="producto-img">
+                <img src="${producto.imagen}"> 
+            </div>
+            <div class="producto-descripcion">
+                <p>${producto.nombre} (x${producto.cantidad})</p>
+                <div class="producto-botones">
+                    <button class="eliminar-producto">-</button>
+                    <p class="cantidad--producto">${producto.cantidad}</p>
+                    <button class="sumar-producto">+</button>
+                </div>
+            </div>
+            <div class="producto-precio"><p>$${producto.precio * producto.cantidad}</p></div>
         `;
         //CREAMOS EL BOTON ELIMINAR PRODUCTO
-        const botonEliminar = div.querySelector('.eliminar-producto');
+        const botonEliminar = divProducto.querySelector('.eliminar-producto');
         botonEliminar.addEventListener('click', () => eliminarProducto(index));
-        contenedorCarrito.appendChild(div);
+        const botonSumar = divProducto.querySelector('.sumar-producto');
+        botonSumar.addEventListener('click', () => {
+            carrito[index].cantidad++; 
+            actualizarCarrito(); 
+        });
+        contenedorCarrito.appendChild(divProducto);
     });
 
     // CREAMOS Y MOSTRAMOS EL TOTAL DE LOS PRODUCTOS
     const total = carrito.reduce((acu, item) => acu + item.precio * item.cantidad, 0);
     const totalDiv = document.createElement('div');
-    totalDiv.classList.add('total');
-    totalDiv.innerHTML = `<h4>Total: $${total}</h4>`;
+    totalDiv.classList.add('carrito-total');
+    totalDiv.innerHTML = `
+                        <div>
+                            <p>Subtotal</p>    
+                        </div>
+                        <div>
+                            <span>$${total}</span>    
+                        </div>
+                        `;
     contenedorCarrito.appendChild(totalDiv);
+
+    //LINEA SEPARADORA
+    const linea=document.createElement('div');
+    linea.classList.add("linea");
+    contenedorCarrito.appendChild(linea);
 
     // CREAMOS EL BOTON VACIAR CARRITO
     const divBtnVaciar= document.createElement('div');
     divBtnVaciar.classList.add("btn-vaciar")
-    divBtnVaciar.innerHTML=`<button>Vaciar Carrito</button>`
+    divBtnVaciar.innerHTML=`<button>VACIAR CARRITO</button>`
     divBtnVaciar.addEventListener('click', vaciarCarrito);
     contenedorCarrito.appendChild(divBtnVaciar);
+
+    //CREAMOS BOTON FINALIZAR COMPRA
+    const divBtnComprar= document.createElement('div');
+    divBtnComprar.classList.add("btn-comprar")
+    divBtnComprar.innerHTML=`<button>REALIZAR COMPRA</button>`
+    divBtnComprar.addEventListener('click', finalizarCompra);
+    contenedorCarrito.appendChild(divBtnComprar);
     
     guardarCarritoEnLocalStorage(); 
+}
+
+function finalizarCompra(){
+    if(carrito.length!=0){
+        Swal.fire({
+            title: "Compra realizada!",
+            text: "Gracias por comprar en Materos!",
+            icon: "success",
+        });
+        vaciarCarrito();
+    }
 }
 
 function eliminarProducto(index) { //ELIMINAMOS LOS PRODUCTOS CON EL -, REDUCE LA CANTIDAD Y CUANDO LLEGA A 0 SE ELIMINA.
@@ -106,13 +181,18 @@ function eliminarProducto(index) { //ELIMINAMOS LOS PRODUCTOS CON EL -, REDUCE L
     actualizarCarrito(); 
 }
 
+function sumarProducto(index) { //SUMAR PRODUCTOS
+    carrito[index].cantidad++; 
+    actualizarCarrito(); 
+}
+
 function vaciarCarrito() { //VACIAMOS LA LISTA DEL CARRITO PARA CUNADO EL BOTON VACIAR CARRITO SEA ACTIVADO
     carrito = []; 
     actualizarCarrito(); 
     guardarCarritoEnLocalStorage(); 
 }
 
-const abrirCarrito = document.querySelector(".abrir"); // FUNCIONALIDAD PARA ABRIR Y CERRAR EL CARRITO
+const abrirCarrito = document.querySelector(".abrir"); // FUNCIONALIDAD PARA ABRIR
 abrirCarrito.addEventListener("click", function() {
     const carritoContenedor = document.getElementById('carrito-contenedor');
     carritoContenedor.classList.toggle('oculto'); 
@@ -124,9 +204,8 @@ function guardarCarritoEnLocalStorage() {
 }
 
 // CUANDO CARGA LA PAG SE RECUPERA EL CARRITO DEL LOCALSTORAGE
-document.addEventListener('DOMContentLoaded', () => {
-    cargarCarritoDesdeLocalStorage();
-});
+cargarCarritoDesdeLocalStorage();
+
 
 
 
